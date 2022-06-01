a58dc7d736786f5a5d074f0097fd7d07385dadc8
simransdsu <simran5590@gmail.com>
2022-06-01 15:30:08 -0700

    Initial Commit

---
+++ World Scramble/World_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  World_ScrambleApp.swift
+//  World Scramble
+//
+//  Created by Simran Preet Singh Narang on 2022-06-01.
+//
+
+import SwiftUI
+
+@main
+struct World_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
