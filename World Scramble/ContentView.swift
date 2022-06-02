7cb8ce3e696057c880d0beff0efe850d8b6621e9
Simran Preet Singh <simran5590@gmail.com>
2022-06-02 07:49:35 -0700

    Added game logic

--- World Scramble/ContentView.swift
+++ World Scramble/ContentView.swift
@@ -9,10 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var words: [String] = []
     @State var usedWords = [String]()
     @State var rootWord = ""
     @State var newWord = ""
+    
+    @State var errorTitle = ""
+    @State var errorMessage = ""
+    @State var showingError = false
     
     var body: some View {
         NavigationView {
@@ -36,6 +39,12 @@ struct ContentView: View {
             .onSubmit {
                 addNewWord()
             }
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
         }
             
     }
@@ -46,17 +55,67 @@ struct ContentView: View {
         guard answer.count > 0 else { return }
         
         // Extra validation to come
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
         
-        usedWords.insert(answer, at: 0)
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recogonized", message: "You cannot just make them up.")
+            return
+        }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
         newWord = ""
     }
     
-    func loadFile() {
+    func startGame() {
         
         guard let fileUrl = Bundle.main.url(forResource: "start", withExtension: "txt"),
-              let fileContents = try? String(contentsOf: fileUrl) else { return }
+              let fileContents = try? String(contentsOf: fileUrl) else {
+            fatalError("Could not load start.txt from bundle.")
+        }
         
-        self.words = fileContents.components(separatedBy: "\n")
+        let allWords = fileContents.components(separatedBy: "\n")
+        rootWord = allWords.randomElement() ?? "silkworm"
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 
