bdc2b427898eae9383cddc95aa5fa77ad746396d
Simran Preet Singh <simran5590@gmail.com>
2022-06-02 07:22:35 -0700

    Adding a word to the list

--- World Scramble/ContentView.swift
+++ World Scramble/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var words: [String] = []
+    @State var usedWords = [String]()
+    @State var rootWord = ""
+    @State var newWord = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                        .disableAutocorrection(true)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit {
+                addNewWord()
+            }
+        }
+            
+    }
+    
+    func addNewWord() {
+        
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        // Extra validation to come
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+    
+    func loadFile() {
+        
+        guard let fileUrl = Bundle.main.url(forResource: "start", withExtension: "txt"),
+              let fileContents = try? String(contentsOf: fileUrl) else { return }
+        
+        self.words = fileContents.components(separatedBy: "\n")
     }
 }
 
